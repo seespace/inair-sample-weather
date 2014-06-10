fb9adbd5101daeb81c46139f24c845c6a1fd007a
Vu Anh <theanh@seespace.co>
2014-06-10 16:50:39 +0700

    * Bug fix

--- inAiR-weather/src/main/java/tv/inair/weather/ForecastLayout.java
+++ inAiR-weather/src/main/java/tv/inair/weather/ForecastLayout.java
@@ -2,14 +2,13 @@ package tv.inair.weather;
 
 import android.os.Bundle;
 
-import inair.app.IABaseLayout;
 import inair.app.IAChildLayout;
 import inair.view.UITextView;
 
 /**
  * Created by Synyster on 6/2/14.
  */
-public class ForecastLayout extends IABaseLayout {
+public class ForecastLayout extends IAChildLayout {
   @Override
   public void onInitialize(Bundle bundle) {
     setRootContentView(R.layout.forecast_item);
