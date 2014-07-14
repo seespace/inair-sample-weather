88d46126069a0a333bc32e5d7166d51c68e42b6e
Linh <linh@seespace.co>
2014-07-15 01:11:15 +0700

    * fix gradle

--- inAiR-weather/src/main/java/tv/inair/weather/view/RootLayout.java
+++ inAiR-weather/src/main/java/tv/inair/weather/view/RootLayout.java
@@ -2,18 +2,16 @@ package tv.inair.weather.view;
 
 import android.os.Bundle;
 
-import inair.app.IABaseRootLayout;
+import inair.app.IARootLayout;
 import tv.inair.weather.Application;
 import tv.inair.weather.R;
 
 /**
  * Created by Synyster on 6/10/14.
  */
-public class RootLayout extends IABaseRootLayout {
+public class RootLayout extends IARootLayout {
   @Override
   public void onInitialize(Bundle bundle) {
     setAndBindRootContentView(R.layout.layered_view, Application.mainViewModel);
-//    setRootContentView(new UIViewGroup(this));
-//    addChildLayout(new LayeredNavigationLayout());
   }
 }
